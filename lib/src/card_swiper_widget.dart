66c215c84be8a9d33fdd720444f32d74404b15d4
marianozamora <zamoramariano@hotmail.com>
2020-01-14 11:28:21 -0300

    resize posters

--- lib/src/card_swiper_widget.dart
+++ lib/src/card_swiper_widget.dart
@@ -18,7 +18,7 @@ class CardSwiper extends StatelessWidget {
       width:double.infinity,
       child: Swiper(
         layout: SwiperLayout.STACK,
-        itemWidth: _screenSize.width * 0.5,
+        itemWidth: _screenSize.width * 0.6,
         itemHeight: _screenSize.height *0.5,
         itemBuilder: (BuildContext context,int index){
           return ClipRRect(
